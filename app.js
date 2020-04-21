6ffafefba92a8e97c87b4a49e964e649a8e4d32d
Emir Matinez <emirmartinez@gmail.com>
2020-04-20 17:38:26 -0700

    Completed basic requirements

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var change = true;
 var winner = false;
 
-
+// EVENT LISTENER
 var clickEventListener = function (location) {
   if (change === true) {
     document.getElementById(location).innerHTML = "X";
@@ -14,55 +14,74 @@ var clickEventListener = function (location) {
   }
 };
 
+// BUTTON DIV
+var buttonHide = function () {
+  var btnDiv = document.getElementById('button');
+  if(winner === true) {
+    btnDiv.style.display = "none"
+  } else {
+    btnDiv.style.display = "block"
+  }
+}
+
+
+// GAME RULES
 var rules = function (user) {
   if (document.getElementById('main').rows[0].cells[0].innerHTML === user &&
   document.getElementById('main').rows[0].cells[1].innerHTML === user &&
   document.getElementById('main').rows[0].cells[2].innerHTML === user) {
     setTimeout(function(){ alert("The winner is " + user); }, 200);
     var winner = true;
-    console.log(winner)
+    buttonHide()
   }
   else if(document.getElementById('main').rows[1].cells[0].innerHTML === user &&
   document.getElementById('main').rows[1].cells[1].innerHTML === user &&
   document.getElementById('main').rows[1].cells[2].innerHTML === user){
     setTimeout(function(){ alert("The winner is " + user); }, 200);
     var winner = true;
+    buttonHide()
   }
   else if(document.getElementById('main').rows[2].cells[0].innerHTML === user &&
   document.getElementById('main').rows[2].cells[1].innerHTML === user &&
   document.getElementById('main').rows[2].cells[2].innerHTML === user){
     setTimeout(function(){ alert("The winner is " + user); }, 200);
     var winner = true;
+    buttonHide()
   }
   else if(document.getElementById('main').rows[0].cells[0].innerHTML === user &&
   document.getElementById('main').rows[1].cells[0].innerHTML === user &&
   document.getElementById('main').rows[2].cells[0].innerHTML === user){
     setTimeout(function(){ alert("The winner is " + user); }, 200);
     var winner = true;
+    buttonHide()
   }
   else if(document.getElementById('main').rows[0].cells[1].innerHTML === user &&
   document.getElementById('main').rows[1].cells[1].innerHTML === user &&
   document.getElementById('main').rows[2].cells[1].innerHTML === user){
     setTimeout(function(){ alert("The winner is " + user); }, 200);
     var winner = true;
+    buttonHide()
   }
   else if(document.getElementById('main').rows[0].cells[2].innerHTML === user &&
   document.getElementById('main').rows[1].cells[2].innerHTML === user &&
   document.getElementById('main').rows[2].cells[2].innerHTML === user){
     setTimeout(function(){ alert("The winner is " + user); }, 200);
     var winner = true;
+    buttonHide()
   }
   else if(document.getElementById('main').rows[0].cells[0].innerHTML === user &&
   document.getElementById('main').rows[1].cells[1].innerHTML === user &&
   document.getElementById('main').rows[2].cells[2].innerHTML === user){
     setTimeout(function(){ alert("The winner is " + user); }, 200);
     var winner = true;
+    buttonHide()
   }
   else if(document.getElementById('main').rows[0].cells[2].innerHTML === user &&
   document.getElementById('main').rows[1].cells[1].innerHTML === user &&
   document.getElementById('main').rows[2].cells[0].innerHTML === user){
     setTimeout(function(){ alert("The winner is " + user); }, 200);
     var winner = true;
+    buttonHide()
   }
 }
 
